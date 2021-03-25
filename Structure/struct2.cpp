98e0fc956f969292149dc85f56bc73f8b54ba01e
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-03-25 11:26:00 +0530

    structur data security using acess specifier

---
+++ Structure/struct2.cpp
@@ -0,0 +1,45 @@
+//CPP:THIRDD DIFFRENCE OF STRUCTURE WITH C(DATA SECURITY USING ACESS SPECIFIER)
+#include<iostream>
+using namespace std;
+#include<conio.h>
+
+struct book
+{
+    private:
+    int bid;
+    char bname[20];
+    float bprice;
+    public:
+    void input()
+    {
+        cout<<"Enter Book id,Book Name and Book Price:";
+        cin>>bid>>bname>>bprice;
+
+    }
+
+    void display()
+    {
+        cout<<"\n"<<"Book Id="<<bid<<" "<<"Book Name="<<bname<<" "<<"Book Price="<<" "<<bprice;
+        if(bid<0)
+        bid=-bid;
+    }
+
+};
+
+int main()
+{
+    book b1;
+    //b1.bid=-101;
+    b1.input();
+    b1.display();
+    getch();
+
+}
+
+/*OUTPUT:
+Enter Book id,Book Name and Book Price:1
+CPP
+9900
+
+Book Id=1 Book Name=CPP Book Price= 9900
+*/
