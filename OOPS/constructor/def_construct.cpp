ec79d5ad131bb70f056606424b643b5d3fb048be
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-03-26 11:21:16 +0530

    constructor

---
+++ OOPS/constructor/def_construct.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Complex
+{
+   public:
+   //DEFAULT CONSTRUCTOR:
+   Complex()
+   {
+    cout<<"Default constructor callled...";
+   }
+
+
+};
+int main()
+{
+    Complex c1;
+}
+
+/*OUTPUT:
+Default constructor callled...
+*/
