552a4416e3afe07564bc6d167a69b019e8d0195a
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-03-27 12:22:00 +0530

    Friend func with operator oveerloading

---
+++ OOPS/Friend function/frnd_func_with_opr_overloading.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class Complex
+{
+    private:
+    int a,b;
+
+    public:
+    void setData(int x,int y)
+    {
+        a=x;
+        b=y;
+    }
+
+
+    void showData()
+    {
+        cout<<"a="<<a<<" "<<"b="<<b;
+    }
+
+    friend Complex operator+(Complex,Complex);
+
+};
+
+Complex operator+(Complex X,Complex Y)
+{
+    Complex temp;
+    temp.a=X.a+Y.a;
+    temp.b=X.b+Y.b;
+    return(temp);
+}
+
+
+int main()
+{
+    Complex c1,c2,c3;
+    c1.setData(5,6);
+    c2.setData(10,6);
+    c3=c1+c2;
+    c3.showData();
+}
+
+/*OUTPUT:
+a=15 b=12
+*/
