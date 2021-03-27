c95def859c3e1eb41e632547cd5eb016c920c588
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-03-27 12:01:27 +0530

    frind func class for more than one class

---
+++ OOPS/Friend function/frieend2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class A
+{
+ private:
+ int a;
+
+ public:
+ void setData(int x)
+ {
+     a=x;
+ }
+ friend void fun(A,B);
+};
+
+class B
+{
+private:
+int b;
+
+public:
+void setData(int y)
+{
+    b=y;
+}
+friend void fun(A,B);
+};
+
+void fun(A o1,B o2)
+{
+ cout<<"Sum="<<o1.a+o2.b;
+}
+
+int main()
+{
+    A obj1;
+    B obj2;
+    obj1.setData(5);
+    obj2.setData(5);
+    fun(obj1,obj2);
+    getch();
+}
